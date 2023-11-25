b6ed2aefd3bb608ae04dae6d38a3a46e74600abd
Annaelle Emery <annaelleemery512@gmail.com>
2023-11-25 18:16:45 +0000

    js try 1

--- assets/script.js
+++ assets/script.js
@@ -1,28 +1,21 @@
 const slides = [
-    {
-        image: "slide1.jpg",
-        tagLine:
-            "Impressions tous formats <span>en boutique et en ligne</span>",
-    },
-    {
-        image: "slide2.jpg",
-        tagLine:
-            "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
-    },
-    {
-        image: "slide3.jpg",
-        tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
-    },
-    {
-        image: "slide4.png",
-        tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
-    },
+    "./assets/images/slideshow/slide1.jpg",
+    "./assets/images/slideshow/slide2.jpg",
+    "./assets/images/slideshow/slide3.jpg",
+    "./assets/images/slideshow/slide4.jpg",
+];
+
+const taglines = [
+    "Impressions tous formats <span>en boutique et en ligne</span>",
+    "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
+    "Grand choix de couleurs <span>de CMJN aux pantones</span>",
+    "Autocollants <span>avec découpe laser sur mesure</span>",
 ];
 
 var slideIdx = 0;
 
 function initslider() {
-    document.querySelectorAll(".cat").forEach(function (elt) {
+    document.querySelectorAll(".arrow").forEach(function (elt) {
         elt.addEventListener("click", function (event) {
             if (event.target.classList.contains("arrow-left")) {
                 console.log("LEFT");
